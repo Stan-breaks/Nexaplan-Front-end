64a0679af820fb00cfc7c6c2850745db352c52ac
Stan-breaks <108292766+Stan-breaks@users.noreply.github.com>
2023-10-03 22:51:01 +0300

    implemetation of a loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,8 @@
+import './Loader.css'
+
+function Loader() {
+  return (
+    <div className="loader"></div>
+  )
+}
+export default Loader
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import Loader from "../components/Loader/Loader"
 
 function Home() {
   return (
     <>
     <h2>Home page</h2>
+    <Loader/>
     </>
   )
 }
