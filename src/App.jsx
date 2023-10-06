daa2fe90f11614286929e53f009b66cde4e4ef0e
Stan-breaks <108292766+Stan-breaks@users.noreply.github.com>
2023-10-06 21:52:50 +0300

    login page initilazation

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/home";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import NotFound from "./pages/NotFound";
-
+import Login from "./pages/Authentication/Login";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -15,7 +15,7 @@ function App() {
         <Navbar />
         <div className="app">
           <Routes>
-            <Route index element={<Home />}></Route>
+            <Route index element={<Login/>}></Route>
             <Route path="/home" element={<Home />}></Route>
             <Route element={<NotFound />}></Route>
           </Routes>
---
+++ src/pages/Authentication/Login.jsx
@@ -0,0 +1,68 @@
+import "./Authentication.css"
+function Login() {
+  const validateEmail = (email) => {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+};
+  const handleClick=(e)=>{
+    e.preventDefault();
+    const email=document.getElementById("exampleInputEmail1").value;
+    const password=document.getElementById("exampleInputPassword1").value;
+    if (
+      password == ""||!validateEmail(email)
+    ) {
+      document.querySelector(".error").style.display = "block";
+    } else {
+      window.location.href = "/home";
+    }
+      
+  }
+  const registerClick=(e)=>{
+   e.preventDefault()
+   window.location.href="/register";
+  }
+  return (
+    <>
+      <h3 className="pageHeader">Login Page</h3>
+      <form>
+        <div className="error">
+          <h4>Invalid email or password</h4>
+          <br />
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" class="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <br />
+        <button class="btn btn-primary register" onClick={registerClick}>
+          Register
+        </button>
+        <button type="submit" class="btn btn-primary" onClick={handleClick}>
+          Submit
+        </button>
+      </form>
+    </>
+  );
+}
+export default Login
