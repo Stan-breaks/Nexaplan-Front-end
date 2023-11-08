8514a88da1c4de837a84642c50af08a32ef4c113
Stan-breaks <108292766+Stan-breaks@users.noreply.github.com>
2023-11-08 20:58:38 +0300

    con

--- src/pages/Collaborator/ProjectPage.jsx
+++ src/pages/Collaborator/ProjectPage.jsx
@@ -4,18 +4,27 @@ import { useSelector } from "react-redux";
 
 function ProjectPage() {
   const [project,setProject]=useState({});
-  const [newTask,setNewTask]=useState({name:"",description:"",dueDate:"",priority:"",assigned:""})
+  const [newTask,setNewTask]=useState({name:"",description:"",dueDate:"",priority:"",assignedTo:""})
   const [newCollaborator, setNewCollaborator] = useState("");
-  const[people,setPeople]=useState([])
+  const [people, setPeople] = useState([]);
   const id=useSelector(state=>state.id.id);
-
+  const[taskForm,setTaskForm]=useState(false)
     
   const handleAddTask = () => {
-    document.querySelector(".addTask").style.display='block';
-    console.log('stan');
+    setTaskForm(true)
   };
   const handleSubmit=()=>{
-
+    const data={...newTask}
+    fetch(`http://127.0.0.1:8000/projectTasks`,{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(data)
+    })
+    .then(response=>response.json())
+    .then(response=>{ console.log(response)})
+    setTaskForm(false)
   }
   const handleAddCollaborator = () => {
     setNewCollaborator("");
@@ -45,71 +54,93 @@ function ProjectPage() {
               <li key={index}>{task}</li>
             ))}
         </ul>
-        <div className="projectTask">
-          <br/>
-          <label htmlFor="text1" className="form-label">
-            Task name
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="text1"
-            value={newTask.name}
-            onChange={(e) => setNewTask({...newTask,name:e.target.value})}
-            required
-          />
-        
-        <div className="mb-3">
-          <label htmlFor="description" className="form-label">
-            Description
-          </label>
-          <textarea
-            className="form-control"
-            id="description"
-            value={newTask.description}
-            onChange={(e) => setNewTask({...newTask,description:e.target.value})}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="dueDate" className="form-label">
-            Due Date
-          </label>
-          <input
-            type="date"
-            className="form-control"
-            id="dueDate"
-            value={newTask.dueDate}
-            onChange={(e) => setNewTask({...newTask,dueDate:e.target.value})}
-            required
-          />
-        </div>
-        <div className="mb-3 form-check">
-          <input
-            type="checkbox"
-            className="form-check-input"
-            id="exampleCheck1"
-            checked={newTask.priority}
-            onChange={(e) => setNewTask({...newTask,priority:e.target.checked})}
-          />
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            prioritize
-          </label>
-          <br/>
+        {taskForm && (
+          <div className="projectTask">
+            <br />
+            <label htmlFor="text1" className="form-label">
+              Task name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="text1"
+              value={newTask.name}
+              onChange={(e) => setNewTask({ ...newTask, name: e.target.value })}
+              required
+            />
+
+            <div className="mb-3">
+              <label htmlFor="description" className="form-label">
+                Description
+              </label>
+              <textarea
+                className="form-control"
+                id="description"
+                value={newTask.description}
+                onChange={(e) =>
+                  setNewTask({ ...newTask, description: e.target.value })
+                }
+                required
+              />
+            </div>
+
+           <div className="mb-3">
+              <label htmlFor="assignedTo" className="form-label">
+                Assigned To
+              </label>
+              <select className="form-select" aria-label="Default select example" id="assignedTo" value={newTask.assignedTo} onChange={(e)=>setNewTask({...newTask,assignedTo:e.target.value})}>
+                <option selected>Open this select menu</option>
+                {project.collaborators && project.collaborators.map((collaborator,index)=><option key={index} value={collaborator}>{collaborator}</option>)}
+              </select>
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="dueDate" className="form-label">
+                Due Date
+              </label>
+              <input
+                type="date"
+                className="form-control"
+                id="dueDate"
+                value={newTask.dueDate}
+                onChange={(e) =>
+                  setNewTask({ ...newTask, dueDate: e.target.value })
+                }
+                required
+              />
+            </div>
+            <div className="mb-3 form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+                checked={newTask.priority}
+                onChange={(e) =>
+                  setNewTask({ ...newTask, priority: e.target.checked })
+                }
+              />
+              <label className="form-check-label" htmlFor="exampleCheck1">
+                prioritize
+              </label>
+              <br />
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
           </div>
-          <button
-          type="submit"
-          className="btn btn-primary"
-          onClick={handleSubmit}
-        >
-          Submit
+        )}
+
+        <button onClick={handleAddTask} className="addTask">
+          Add Task
         </button>
-        </div>
-        <button onClick={handleAddTask} className="addTask">Add Task</button>
         <p>Status: {project.projectStatus ? "Active" : "Inactive"}</p>
         <p>
           Collaborators:
-          {project.collaborators<0? project.collaborators.join(", "):"0"}
+          {project.collaborators < 0 ? project.collaborators.join(", ") : "0"}
         </p>
         {people && (
           <select
