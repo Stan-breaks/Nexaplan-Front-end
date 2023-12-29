b7d93fff8c5ac2ab6fedd5a8f25ad1da9755cd3e
Stan-breaks <stanleymwendwa03@gmail.com>
2023-12-29 17:03:42 +0300

    update readme file

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
-# React + Vite
+# Nexaplan
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Nexaplan is a project and task management system frontend implemented using React and Vite.
 
-Currently, two official plugins are available:
+## Table of Contents
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Contributing](#contributing)
+
+## Installation
+
+Before you start, make sure you have a recent version of Node.js installed.
+
+```bash
+git clone https://github.com/yourusername/nexaplan.git
+cd nexaplan
+npm install
+```
+
+## Usage
+
+To start the development server:
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- src/pages/Collaborator/ProjectPage.jsx
+++ src/pages/Collaborator/ProjectPage.jsx
@@ -3,242 +3,252 @@ import Layout from "../Layout";
 import { useSelector } from "react-redux";
 
 function ProjectPage() {
-  const [project,setProject]=useState({});
-  const [newTask,setNewTask]=useState({name:"",description:"",dueDate:"",priority:"",assignedTo:"",category:""})
+  const [project, setProject] = useState({});
+  const [newTask, setNewTask] = useState({
+    name: "",
+    description: "",
+    dueDate: "",
+    priority: "",
+    assignedTo: "",
+    category: "",
+  });
   const [newCollaborator, setNewCollaborator] = useState("");
   const [people, setPeople] = useState([]);
-  const id=useSelector(state=>state.id.id);
-  const[taskForm,setTaskForm]=useState(false);
-  const[comment,setComment]= useState(false); 
-  const[comments,setComments]=useState({})
-  const handleSubmit=()=>{
-    const data={...newTask,category:project.projectName}
-    fetch(`http://127.0.0.1:8000/projectTasks/${id}`,{
-      method:"POST",
-      headers:{
-        "Content-Type":"application/json"
+  const id = useSelector((state) => state.id.id);
+  const [taskForm, setTaskForm] = useState(false);
+  const [comment, setComment] = useState(false);
+  const [comments, setComments] = useState({});
+  const handleSubmit = () => {
+    const data = { ...newTask, category: project.projectName };
+    fetch(`http://127.0.0.1:8000/projectTasks/${id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-      body:JSON.stringify(data)
+      body: JSON.stringify(data),
     })
-    .then(response=>response.json())
-    .then(response=>{ console.log(response)})
-     setTimeout(() => {
-       alert("Task Added");
-       setTaskForm(false);
-       location.reload();
-     }, 1000);
-  
-  }
-  const handleProjectClick=()=>{
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+      });
+    setTimeout(() => {
+      alert("Task Added");
+      setTaskForm(false);
+      location.reload();
+    }, 1000);
+  };
+  const handleProjectClick = () => {
     fetch(`http://127.0.0.1:8000/projectHandling/${id}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body:JSON.stringify({"message":"delete"})
+      body: JSON.stringify({ message: "delete" }),
     });
-    setTimeout(()=>{
-      alert("Project Status Flipped")
+    setTimeout(() => {
+      alert("Project Status Flipped");
       location.reload();
-    },1000)
-  }
+    }, 1000);
+  };
   const handleAddCollaborator = () => {
-    const data = { collaborator: newCollaborator,};
-    fetch(`http://127.0.0.1:8000/projectCollaborators/${id}`,{
-      method:"POST",
-      headers:{
-        "Content-Type":"application/json"
+    const data = { collaborator: newCollaborator };
+    fetch(`http://127.0.0.1:8000/projectCollaborators/${id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-      body:JSON.stringify(data)
-    })
+      body: JSON.stringify(data),
+    });
     setTimeout(() => {
       alert("Collaborator Added");
-      location.reload()
+      location.reload();
     }, 1000);
-    
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch(`http://127.0.0.1:8000/projectView/${id}`)
-    .then(response=>response.json())
-    .then(response=>{
-        
-        setProject(response)
-    })
+      .then((response) => response.json())
+      .then((response) => {
+        setProject(response);
+      });
     fetch(`http://127.0.0.1:8000/usersList/${id}`)
-    .then(response=>response.json())
-    .then(response=>{
-      setPeople(response)
-    })
+      .then((response) => response.json())
+      .then((response) => {
+        setPeople(response);
+      });
     fetch(`http://127.0.0.1:8000/comment${id}`)
-    .then(response=>response.json())
-    .then(response=>{
-        setComments(response)
-      })
-  },[])
+      .then((response) => response.json())
+      .then((response) => {
+        setComments(response);
+      });
+  }, []);
   console.log(project);
   return (
     <Layout>
-      {comment?
-      <>
-      <button onClick={()=>setComment(false)}>Project</button>
-      <div className="Comments">
-      <h3>Comments</h3>
-      {comments && comments.map((data)=>{
-            return <>
-                <p>
-              
-               </p></>
-            })}
-      </div>
-      </>
-      :
-      <>
-      <button onClick={()=>setComment(true)}>Comments</button>
-      <div className="project">
-        <h2>{project.projectName}</h2>
-        <p>{project.projectDescription}</p>
-        <ul>
-          <h2>Project tasks</h2>
-          {project.projectTask == 0 && <p>No tasks here</p>}
-          {project.projectTask &&
-            project.projectTask.map((task, index) => (
-              <>
-                <li key={index} className="category">
-                  {task}
-                </li>
-              </>
-            ))}
-        </ul>
-        {taskForm && (
-          <div className="projectTask">
-            <br />
-            <label htmlFor="text1" className="form-label">
-              Task name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="text1"
-              value={newTask.name}
-              onChange={(e) => setNewTask({ ...newTask, name: e.target.value })}
-              required
-            />
+      {comment ? (
+        <>
+          <button onClick={() => setComment(false)}>Project</button>
+          <div className="Comments">
+            <h3>Comments</h3>
+            {comments &&
+              comments.map((data) => {
+                return (
+                  <>
+                    <p></p>
+                  </>
+                );
+              })}
+          </div>
+        </>
+      ) : (
+        <>
+          <button onClick={() => setComment(true)}>Comments</button>
+          <div className="project">
+            <h2>{project.projectName}</h2>
+            <p>{project.projectDescription}</p>
+            <ul>
+              <h2>Project tasks</h2>
+              {project.projectTask == 0 && <p>No tasks here</p>}
+              {project.projectTask &&
+                project.projectTask.map((task, index) => (
+                  <>
+                    <li key={index} className="category">
+                      {task}
+                    </li>
+                  </>
+                ))}
+            </ul>
+            {taskForm && (
+              <div className="projectTask">
+                <br />
+                <label htmlFor="text1" className="form-label">
+                  Task name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="text1"
+                  value={newTask.name}
+                  onChange={(e) =>
+                    setNewTask({ ...newTask, name: e.target.value })
+                  }
+                  required
+                />
 
-            <div className="mb-3">
-              <label htmlFor="description" className="form-label">
-                Description
-              </label>
-              <textarea
-                className="form-control"
-                id="description"
-                value={newTask.description}
-                onChange={(e) =>
-                  setNewTask({ ...newTask, description: e.target.value })
-                }
-                required
-              />
-            </div>
+                <div className="mb-3">
+                  <label htmlFor="description" className="form-label">
+                    Description
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="description"
+                    value={newTask.description}
+                    onChange={(e) =>
+                      setNewTask({ ...newTask, description: e.target.value })
+                    }
+                    required
+                  />
+                </div>
 
-            <div className="mb-3">
-              <label htmlFor="assignedTo" className="form-label">
-                Assigned To
-              </label>
+                <div className="mb-3">
+                  <label htmlFor="assignedTo" className="form-label">
+                    Assigned To
+                  </label>
+                  <select
+                    className="form-select"
+                    aria-label="Default select example"
+                    id="assignedTo"
+                    value={newTask.assignedTo}
+                    onChange={(e) =>
+                      setNewTask({ ...newTask, assignedTo: e.target.value })
+                    }
+                  >
+                    <option selected>Open this select menu</option>
+                    {project.collaborators &&
+                      project.collaborators.map((collaborator, index) => (
+                        <option key={index} value={collaborator}>
+                          {collaborator}
+                        </option>
+                      ))}
+                  </select>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="dueDate" className="form-label">
+                    Due Date
+                  </label>
+                  <input
+                    type="date"
+                    className="form-control"
+                    id="dueDate"
+                    value={newTask.dueDate}
+                    onChange={(e) =>
+                      setNewTask({ ...newTask, dueDate: e.target.value })
+                    }
+                    required
+                  />
+                </div>
+                <div className="mb-3 form-check">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    id="exampleCheck1"
+                    checked={newTask.priority}
+                    onChange={(e) =>
+                      setNewTask({ ...newTask, priority: e.target.checked })
+                    }
+                  />
+                  <label className="form-check-label" htmlFor="exampleCheck1">
+                    prioritize
+                  </label>
+                  <br />
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-primary"
+                  onClick={handleSubmit}
+                >
+                  Submit
+                </button>
+              </div>
+            )}
+
+            {taskForm ? (
+              <button onClick={() => setTaskForm(false)}>Cancel</button>
+            ) : (
+              <button onClick={() => setTaskForm(true)}>Add Task</button>
+            )}
+            <p>Status: {project.projectStatus ? "Active" : "Inactive"}</p>
+            <p>
+              Collaborators:
+              {project.collaborators ? project.collaborators.join(", ") : "0"}
+            </p>
+
+            {people && (
               <select
-                className="form-select"
-                aria-label="Default select example"
-                id="assignedTo"
-                value={newTask.assignedTo}
-                onChange={(e) =>
-                  setNewTask({ ...newTask, assignedTo: e.target.value })
-                }
+                value={newCollaborator}
+                onChange={(e) => setNewCollaborator(e.target.value)}
               >
                 <option selected>Open this select menu</option>
-                {project.collaborators &&
-                  project.collaborators.map((collaborator, index) => (
-                    <option key={index} value={collaborator}>
-                      {collaborator}
-                    </option>
-                  ))}
+                {people.map((collaborator, index) => (
+                  <option key={index} value={collaborator}>
+                    {collaborator}
+                  </option>
+                ))}
               </select>
-            </div>
-
-            <div className="mb-3">
-              <label htmlFor="dueDate" className="form-label">
-                Due Date
-              </label>
-              <input
-                type="date"
-                className="form-control"
-                id="dueDate"
-                value={newTask.dueDate}
-                onChange={(e) =>
-                  setNewTask({ ...newTask, dueDate: e.target.value })
-                }
-                required
-              />
-            </div>
-            <div className="mb-3 form-check">
-              <input
-                type="checkbox"
-                className="form-check-input"
-                id="exampleCheck1"
-                checked={newTask.priority}
-                onChange={(e) =>
-                  setNewTask({ ...newTask, priority: e.target.checked })
-                }
-              />
-              <label className="form-check-label" htmlFor="exampleCheck1">
-                prioritize
-              </label>
-              <br />
-            </div>
+            )}
+            <button onClick={handleAddCollaborator}>Add Collaborator</button>
+            <br />
             <button
-              type="submit"
-              className="btn btn-primary"
-              onClick={handleSubmit}
+              onClick={handleProjectClick}
+              style={{ backgroundColor: project.projectStatus ? "Red" : "" }}
             >
-              Submit
+              {project.projectStatus ? <>Terminate</> : <>Activate</>}
             </button>
           </div>
-        )}
-
-        {taskForm ? (
-          <button onClick={() => setTaskForm(false)}>Cancel</button>
-        ) : (
-          <button onClick={() => setTaskForm(true)}>Add Task</button>
-        )}
-        <p>Status: {project.projectStatus ? "Active" : "Inactive"}</p>
-        <p>
-          Collaborators:
-          {project.collaborators ? project.collaborators.join(", ") : "0"}
-        </p>
-
-        {people && (
-          <select
-            value={newCollaborator}
-            onChange={(e) => setNewCollaborator(e.target.value)}
-          >
-            <option selected>Open this select menu</option>
-            {people.map((collaborator, index) => (
-              <option key={index} value={collaborator}>
-                {collaborator}
-              </option>
-            ))}
-          </select>
-        )}
-        <button onClick={handleAddCollaborator}>Add Collaborator</button>
-        <br />
-        <button onClick={handleProjectClick}
-        style={{backgroundColor:project.projectStatus?'Red':''}}  
-      
-        >
-          {project.projectStatus ? <>Terminate</> : <>Activate</>}
-        </button>
-      </div>
-      </>
-}
-          </Layout>
+        </>
+      )}
+    </Layout>
   );
 }
 
